c0a921a7d6ea24b6dfe68219f6db9a0fc2bc04fb
stevegcl <sg@chasdeilev.org>
2026-06-05 16:08:01 -0400

    Add start script to package.json

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 {
   "name": "groupme-ai-bot",
   "version": "1.0.0",
-  "description": "",
   "main": "index.js",
   "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
+    "start": "node index.js"
   },
-  "keywords": [],
-  "author": "",
-  "license": "ISC",
   "dependencies": {
     "@anthropic-ai/sdk": "^0.100.1",
     "@google/generative-ai": "^0.24.1",
     "dotenv": "^17.4.2",
-    "express": "^5.2.1",
-    "node-fetch": "^3.3.2"
+    "express": "^5.2.1"
   }
 }
